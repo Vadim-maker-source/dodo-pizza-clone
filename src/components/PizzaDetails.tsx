3b451e2e0d5282b51a6672cdd1db8fceb8d2d93a
Vadim <Vadimbureev380@gmail.com>
2025-07-06 10:41:11 +0300

    small changes fixes

--- src/components/PizzaDetails.tsx
+++ src/components/PizzaDetails.tsx
@@ -199,7 +199,7 @@ export const PizzaDetailsModal = ({
             <div className="flex justify-end mb-4">
               <button 
                 onClick={onClose}
-                className="text-gray-500 hover:text-gray-700 hidden md:block curosr-pointer"
+                className="text-gray-500 hover:text-gray-700 hidden md:block cursor-pointer"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
